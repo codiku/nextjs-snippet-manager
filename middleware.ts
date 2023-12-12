6b114b178525b163eab4d6bafafebe4147b71af0
Codiku <111140836+codiku-dev@users.noreply.github.com>
2023-12-12 17:15:42 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+/*import { NextRequest } from "next/server";
 
 import { authMiddleware } from "@clerk/nextjs";
 
@@ -10,7 +10,7 @@ export default async function middlewares(req: NextRequest) {
     console.log("Navigation on ... ", pathname);
   }
   //@ts-ignore
-  return authMiddleware({})(req);
+ // return authMiddleware({})(req);
 }
 
 export const config = {
@@ -19,3 +19,24 @@ export const config = {
 // evrything but .something and  not containing _next,
 // exactly /
 // /api or /trpc with extra string after
+*/
+
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    const pathname = req.nextUrl.pathname;
+    if (pathname.match("/api/*")) {
+      console.log("Calling api...", pathname);
+    } else if (pathname.match("/snippets/*")) {
+      console.log("Navigation on ... ", pathname);
+    }
+    // Allow users visiting public routes to access them
+    return NextResponse.next();
+  },
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
