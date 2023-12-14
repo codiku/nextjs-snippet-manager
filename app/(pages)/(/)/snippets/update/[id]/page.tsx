b1c6e303ce8ad8d241e3c75a676f0c1a3051cb56
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-14 18:09:25 +0100

    wip

--- app/(pages)/(/)/snippets/update/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { readSnippet } from "@/api/snippets/[id]/service";
-import { FormUpdateSnippet } from "@/components/FormUpdateSnippet";
-
-export default async function UpdateSnippetPage(p: { params: { id: string } }) {
-  const { data: snippet } = await readSnippet(Number(p.params.id));
-  return <FormUpdateSnippet snippet={snippet!} />;
-}
