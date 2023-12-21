c70e6ad726aaae32df2086c10afe3070fc7fe05f
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-21 18:14:05 +0100

    wip

--- app/api/snippet/[id]/service.ts
+++ app/api/snippet/[id]/service.ts
@@ -1,11 +1,24 @@
-import { Snippet } from "@prisma/client";
+import { Language, Snippet, Technology } from "@prisma/client";
 import { db } from "@/app/lib/db";
+import { z } from "zod";
+
+const updateSnippetSchema = z
+  .object({
+    title: z.string().optional(),
+    content: z.string().optional(),
+    language: z.nativeEnum(Language).optional(),
+    technology: z.nativeEnum(Technology).optional(),
+  })
+  .refine((data) => Object.keys(data).length > 0, {
+    message: "At least one value must be provided",
+  });
 
 export async function updateSnippet(
   id: number,
   body: Partial<Omit<Snippet, "id">>
 ) {
   try {
+    updateSnippetSchema.parse(body);
     const updatedSnippet = await db.snippet.update({
       data: body,
       where: { id },
@@ -21,8 +34,11 @@ export async function updateSnippet(
   }
 }
 
+const deleteSnippetSchema = z.number();
+
 export async function deleteSnippet(id: number) {
   try {
+    deleteSnippetSchema.parse(id);
     const deletedSnippet = await db.snippet.delete({
       where: { id },
     });
--- app/api/snippet/service.ts
+++ app/api/snippet/service.ts
@@ -1,16 +1,40 @@
 import { db } from "@/app/lib/db";
-import { Snippet } from "@prisma/client";
+import { Language, Snippet, Technology } from "@prisma/client";
+import { z } from "zod";
 
+const readAllSnippetSchema = z.object({
+  title: z.string().optional(),
+  content: z.string().optional(),
+  language: z.nativeEnum(Language).optional(),
+  technology: z.nativeEnum(Technology).optional(),
+});
 export async function readAllSnippet(filters: Partial<Snippet>) {
-  return await db.snippet.findMany({
-    where: {
-      ...filters,
-    },
-  });
+  try {
+    readAllSnippetSchema.parse(filters);
+    return await db.snippet.findMany({
+      where: {
+        ...filters,
+      },
+    });
+  } catch (err) {
+    return {
+      error: true,
+      status: 500,
+      message:
+        "Something went wrong fetching the snippets " + (err as Error).message,
+    };
+  }
 }
 
+const createSnippetSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  language: z.nativeEnum(Language),
+  technology: z.nativeEnum(Technology),
+});
 export async function createSnippet(body: Omit<Snippet, "id">) {
   try {
+    createSnippetSchema.parse(body);
     const snippetCreated = await db.snippet.create({ data: body });
     return snippetCreated;
   } catch (err) {
