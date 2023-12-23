fdd0451b0c8d46bd692ac96d46b447d737ce0cbd
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-23 18:21:36 +0100

    wip

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,16 +2,18 @@
 import Image from "next/image";
 import logoPng from "/public/logo.png";
 import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 
 export function Header() {
+  const { userId } = useAuth();
   return (
     <header className="bg-main-700 px-8 py-8 flex justify-between">
       <Link href="/">
         <Image src={logoPng} alt="Logo" className="w-48" />
       </Link>
-      <div className="flex items-center space-x-3">
+      <div className="flex items-center space-x-3 text-white">
         <UserButton afterSignOutUrl="/auth/sign-in" />
+        {userId}
       </div>
     </header>
   );
