e5e6820a4617a4705708ee46413daedb4480aa7d
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-27 17:32:45 +0100

    wip

--- app/(pages)/(main)/snippets/technology/[technology]/page.tsx
+++ app/(pages)/(main)/snippets/technology/[technology]/page.tsx
@@ -1,9 +1,17 @@
+import { readAllSnippet } from "@/app/api/snippet/service";
 import { SnippetSearch } from "@/app/components/SnippetSearch/SnippetSearch";
+import { Technology } from "@prisma/client";
 
-export default function TechnologyPage(p: { params: { technology: string } }) {
+export default async function TechnologyPage(p: {
+  params: { technology: Technology };
+}) {
+  const { data: snippets } = await readAllSnippet({
+    technology: p.params.technology,
+  });
   return (
     <div>
       <SnippetSearch
+        snippets={snippets}
         placeholder={`Search your ${p.params.technology} snippets`}
       />
     </div>
