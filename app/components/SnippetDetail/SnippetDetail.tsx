5d234c04d1644ff42fd21b1d160969ae25703cda
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-28 18:38:43 +0100

    wip

--- app/components/SnippetDetail/SnippetDetail.tsx
+++ app/components/SnippetDetail/SnippetDetail.tsx
@@ -2,46 +2,22 @@
 import { SNIPPETS_METADATA } from "@/app/constant";
 import { Snippet } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 import { Prism as SynthaxHighlighter } from "react-syntax-highlighter";
 import { atomDark as theme } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { MdDelete, MdEdit } from "react-icons/md";
+import { MdEdit, MdDelete } from "react-icons/md";
 import { RxCopy } from "react-icons/rx";
-import Link from "next/link";
-import { MouseEvent } from "react";
 import { toast } from "sonner";
+import { MouseEvent } from "react";
 
 export function SnippetDetail(p: { snippet: Snippet }) {
   const snippetMetadata = SNIPPETS_METADATA[p.snippet.technology];
 
-  const copyCodeToClipboard = (e: MouseEvent<HTMLDivElement>) => {
+  const copyCodeIntoClipboard = (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     navigator.clipboard.writeText(p.snippet.content);
-    toast("Code copied into clipboard");
+    toast.info("Code copied into clipboard");
   };
-
-  const actionButtons = (
-    <div className="flex justify-end space-x-4">
-      <Link
-        href={"/snippets/update/" + p.snippet.id}
-        className="icon-box self-end flex flex-col"
-      >
-        <MdEdit />
-        Edit
-      </Link>
-      <div className="icon-box self-end flex flex-col">
-        <MdDelete />
-        Delete
-      </div>
-      <div
-        className="icon-box self-end flex flex-col"
-        onClick={copyCodeToClipboard}
-      >
-        <RxCopy />
-        Copy
-      </div>
-    </div>
-  );
-
   const codeHighLighter = (
     <SynthaxHighlighter
       showLineNumbers
@@ -62,17 +38,25 @@ export function SnippetDetail(p: { snippet: Snippet }) {
     </div>
   );
 
-  const deleteDialog = (
-    <div className="p-8 rounded-lg flex shadow-xl flex-col items-center justify-center absolute top-72 h-56 left-[40%] bg-white">
-      <div className="text-xl font-bold mb-4">Delete snippet</div>
-      <div>Are you sure you want to delete the snippet ?</div>
-      <div className="space-x-20 mt-14">
-        <button className="bg-main-400">Cancel</button>
-        <button className="bg-red-500 hover:bg-red-500/80">Delete</button>
+  const actionButtons = (
+    <div className="flex justify-end space-x-4">
+      <Link
+        href={`/snippets/update/${p.snippet.id}`}
+        className="icon-box flex flex-col"
+      >
+        <MdEdit />
+        Edit
+      </Link>
+      <div className="icon-box flex flex-col">
+        <MdDelete />
+        Delete
+      </div>
+      <div className="icon-box flex flex-col" onClick={copyCodeIntoClipboard}>
+        <RxCopy />
+        Copy
       </div>
     </div>
   );
-
   return (
     <div>
       {title}
@@ -80,7 +64,6 @@ export function SnippetDetail(p: { snippet: Snippet }) {
         {actionButtons}
         {codeHighLighter}
       </div>
-      {deleteDialog}
     </div>
   );
 }
