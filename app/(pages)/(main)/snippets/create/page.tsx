954593a3108af6065a911a788c57320bd7ee99b0
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2024-01-02 15:09:39 +0100

    wip

--- app/(pages)/(main)/snippets/create/page.tsx
+++ app/(pages)/(main)/snippets/create/page.tsx
@@ -1,3 +1,48 @@
-export default function CreateSnippetPage(p: {}) {
-  return <div className="text-white">create</div>;
+"use client";
+import { SNIPPETS_METADATA } from "@/app/constant";
+
+export default function CreateSnippetPage() {
+  const technoSelect = (
+    <div className="space-y-3 w-80">
+      <label htmlFor="technology">Framework / Technology / Language</label>
+      <select id="technology">
+        {Object.keys(SNIPPETS_METADATA).map((techno) => {
+          const { technology: value, label } = SNIPPETS_METADATA[techno];
+          return (
+            <option key={value} value={value}>
+              {label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+
+  const inputTitle = (
+    <div className="space-y-3 w-72">
+      <label htmlFor="title">Title</label>
+      <input id="title" />
+    </div>
+  );
+
+  const textareaContent = (
+    <div className="space-y-3">
+      <label htmlFor="content">Content</label>
+      <textarea id="content" className="h-96 w-full" />
+    </div>
+  );
+
+  return (
+    <form onSubmit={() => alert("Todo")} className="space-y-8 w-[50rem] ">
+      <div className="space-y-6">
+        <h1>New snippet</h1>
+        {inputTitle}
+        {technoSelect}
+        {textareaContent}
+      </div>
+      <div className="flex justify-end">
+        <button>Save</button>
+      </div>
+    </form>
+  );
 }
