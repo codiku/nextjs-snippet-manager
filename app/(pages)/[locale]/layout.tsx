40720d4c84bd1a1b56a0b4e60145ce205b9ae8f1
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-01-09 13:02:14 +0100

    wip

--- app/(pages)/[locale]/layout.tsx
+++ app/(pages)/[locale]/layout.tsx
@@ -5,29 +5,33 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
-
+import { frFR, enUS } from "@clerk/localizations";
 export const metadata: Metadata = {
   title: "Snipia",
   description: "The one place for all your code snippets",
 };
 
-export default function RootLayout({
-  children,
-}: {
+const clerkI18n = {
+  fr: frFR,
+  en: enUS,
+};
+export default function RootLayout(p: {
   children: React.ReactNode;
+  params: { locale: "en" | "fr" };
 }) {
   return (
     <ClerkProvider
+      localization={clerkI18n[p.params.locale]}
       appearance={{
         baseTheme: dark,
         variables: { colorPrimary: "#d44700" },
         elements: { card: "bg-main-700 shadow-none" },
       }}
     >
-      <html lang="en">
+      <html lang={p.params.locale}>
         <body className={inter.className}>
           <Toaster richColors />
-          {children}
+          {p.children}
         </body>
       </html>
     </ClerkProvider>
