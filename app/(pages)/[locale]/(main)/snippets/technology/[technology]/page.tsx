153dc961cde40016cca31085e2404fabdad681d3
Chill-Studio <chill.studio.dev@gmail.com>
2024-06-23 17:46:30 +0200

    client side provider for tech

--- app/(pages)/[locale]/(main)/snippets/technology/[technology]/page.tsx
+++ app/(pages)/[locale]/(main)/snippets/technology/[technology]/page.tsx
@@ -1,6 +1,8 @@
 import { readAllSnippet } from "@/app/api/snippet/service";
 import { SnippetSearch } from "@/app/components/SnippetSearch/SnippetSearch";
 import { Technology } from "@prisma/client";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 
 export default async function TechnologyPage(p: {
   params: { technology: Technology };
@@ -8,12 +10,15 @@ export default async function TechnologyPage(p: {
   const { data: snippets } = await readAllSnippet({
     technology: p.params.technology,
   });
+  const messages = await getMessages();
   return (
     <div>
-      <SnippetSearch
-        snippets={snippets}
-        placeholder={`Search your ${p.params.technology} snippets`}
-      />
+      <NextIntlClientProvider messages={messages}>
+        <SnippetSearch
+          snippets={snippets}
+          placeholder={`Search your ${p.params.technology} snippets`}
+        />
+      </NextIntlClientProvider>
     </div>
   );
 }
